6281c1d9aa66f3143958f5ca6fbd157c80129600
Darshansawant12 <darshansawant1213@gmail.com>
2025-05-20 15:42:51 +0530

    my first commit

---
+++ Myfirstjava/src/package1/Firstprogram.java
@@ -0,0 +1,11 @@
+package package1;
+
+public class Firstprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.print("Hello world");
+       System.out.print("nimish bolte");
+	}
+
+}
