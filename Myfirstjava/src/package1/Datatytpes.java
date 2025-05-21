557ef2262b6016d99472d1e1a109eb451ac85a4e
Darshansawant12 <darshansawant1213@gmail.com>
2025-05-21 09:40:56 +0000

    Datatypes

---
+++ Myfirstjava/src/package1/Datatytpes.java
@@ -0,0 +1,39 @@
+package package1;
+
+public class Datatytpes {
+    public static void main(String[] args) {
+        //boolean example
+        boolean IOT = true;
+        System.out.println(IOT);
+
+        //byte example
+        byte range;
+        range=127;
+        System.out.println(range);
+
+        //short example
+        short range1; 
+        range1 =-127;
+        System.out.println(range1);
+
+        //int example
+        int range2 = 303030303;
+        System.out.println(range2);
+
+        //long example
+        long range3 = 4567894445757l;
+        System.out.println(range3);
+
+        //double example
+        double number=99.9;
+        System.out.println(number);
+
+        //float example
+        float number1=99.9f;
+        System.out.println(number1);
+
+        //char example
+        char letter='\u0041';
+        System.out.println(letter);
+    }
+}
--- Myfirstjava/src/package1/Firstprogram.java
+++ Myfirstjava/src/package1/Firstprogram.java
@@ -3,9 +3,19 @@ package package1;
 public class Firstprogram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-       System.out.print("Hello world");
-       System.out.print("nimish bolte");
+		
+		String name = "Darshan";
+		int age = 20;
+		int mobile = 1234567890;
+		int phone = 987654321;
+		String address = "Nashik";
+		String email = "darshan@gmail.com";
+       System.out.print(name);
+       System.out.print(age);
+	   System.out.print(mobile);
+	   System.out.print(address);
+	   System.out.print(email);
+	   
 	   																											
 
 	}
