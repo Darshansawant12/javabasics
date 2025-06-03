89c7cceaadddfc8a52513371762bd25791277be8
Darshansawant12 <darshansawant1213@gmail.com>
2025-06-03 10:09:14 +0000

    String method

---
+++ package1/Lengthfunctionstringmethod.java
@@ -0,0 +1,18 @@
+package package1;
+
+public class Lengthfunctionstringmethod {
+    public static void main(String[] args) {
+        //String declaration without using new operator
+        String a="Cooking";
+        System.out.println("String a ="+ a);
+        
+        //String declaration using new operator
+        String b = new String("iot");
+        System.out.println("String b=" +b);
+
+        System.out.println("The length of string a is: " + a.length());
+        System.out.println("The length of string b is: " + b.length());
+        System.out.println(a.toUpperCase());
+        System.out.println(b.toLowerCase());
+    }
+}
