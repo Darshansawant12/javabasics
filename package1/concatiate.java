0ba6846b4bc5988d5d859e2f9f9c91a92847f54a
Darshansawant12 <darshansawant1213@gmail.com>
2025-06-04 10:35:25 +0000

    tochararray

--- package1/concatiate.java
+++ package1/concatiate.java
@@ -1,15 +1,35 @@
+// package package1;
+// import java.io.*;
+// public class concatiate {
+
+//     public static void main(String[] args) {
+//         String str1= "darshan";
+//         String str2= "patel";
+//         String str3= "is";
+//         String str4 = str1.concat(str3).concat(str2);
+//         String result = str1 + " " + str3 + " " + str2;
+//         System.out.println("Concat string using concat method: "+ result);
+//         System.out.println("concat: "+ str4);
+//     }
+// }
+
 package package1;
 import java.io.*;
-public class concatiate {
+public class concatiate{
+    public static void main (String[] args){
+        String str1 = "Welcome";
+        String str2 =" to the world of ";
+        String str3 = "Java programming";
 
-    public static void main(String[] args) {
-        String str1= "darshan";
-        String str2= "patel";
-        String str3= "is";
-        String str4 = str1.concat(str3).concat(str2);
-        String result = str1 + " " + str3 + " " + str2;
-        System.out.println("Concat string using concat method: "+ result);
-        System.out.println("concat: "+ str4);
+        String str4 = str1.concat("XYZ");
+        String str5 = str1. concat(str2);
+        String str6 = str1. concat(str2).concat(str3);
+        String str7 = str1 + str2;
+
+        System.out.println(str4);
+        System.out.println(str5);
+        System.out.println(str6);
+        System.out.println(str7);
+
     }
 }
-
